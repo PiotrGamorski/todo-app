f71a538230ee12a527a18256ca6c957385bf53e5
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-08 08:32:42 +0200

    Reports made async with respect to the change of a task

--- src/main/java/io/github/PiotrGamorski/reports/ChangedTaskEventListener.java
+++ src/main/java/io/github/PiotrGamorski/reports/ChangedTaskEventListener.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,11 +21,13 @@ public class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event){
         onChanged(event);
     }
 
+    @Async
     @EventListener
     public void on(TaskUndone event){
         onChanged(event);
