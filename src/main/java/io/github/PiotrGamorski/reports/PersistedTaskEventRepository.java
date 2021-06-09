9441eb37a1c81ff957ccdaf4d3ed4a8d588978d2
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-09 08:51:30 +0200

    Processing reports improved with SQL query for finding all tasks done before deadline

--- src/main/java/io/github/PiotrGamorski/reports/PersistedTaskEventRepository.java
+++ src/main/java/io/github/PiotrGamorski/reports/PersistedTaskEventRepository.java
@@ -1,9 +1,18 @@
 package io.github.PiotrGamorski.reports;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PersistedTaskEventRepository extends JpaRepository<PersistedTaskEvent, Integer> {
     List<PersistedTaskEvent> findByTaskId(int taskId);
+    List<PersistedTaskEvent> findAll();
+
+    @Query(nativeQuery = true, value = "SELECT DISTINCT TASK_EVENTS.ID, TASK_EVENTS.TASK_ID, TASK_EVENTS.NAME, TASK_EVENTS.OCCURRENCE" +
+            " FROM TASKS " +
+            "RIGHT JOIN" +
+            " TASK_EVENTS ON TASK_EVENTS.TASK_ID=TASKS.ID" +
+            " WHERE DEADLINE IS NOT NULL AND DEADLINE <= OCCURRENCE AND NAME='TaskDone' AND TASKS.DONE=TRUE")
+    List<PersistedTaskEvent> findAllDoneBeforeDeadline();
 }
--- src/main/java/io/github/PiotrGamorski/reports/ReportController.java
+++ src/main/java/io/github/PiotrGamorski/reports/ReportController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/reports")
@@ -42,6 +43,8 @@ public class ReportController {
             this.done = task.isDone();
             this.changesCounter = events.size();
         }
+
+
     }
 
     @GetMapping("/deadline/{id}")
@@ -51,6 +54,22 @@ public class ReportController {
                 .map(task -> new TaskDoneBeforeDeadline(task, persistedTaskEventRepository.findByTaskId(id)))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/deadline")
+    List<TaskDoneBeforeDeadline> readTasksDoneBeforeDeadline(){
+
+        var eventsIds = persistedTaskEventRepository.findAll().stream()
+                .map(event -> event.taskId)
+                .distinct()
+                .collect(Collectors.toList());
+
+        var tasks = taskRepository.findAll().stream()
+                .filter(task -> eventsIds.contains(task.getId()))
+                .collect(Collectors.toList());
+
+        return tasks.stream().map(task -> new TaskDoneBeforeDeadline(task, persistedTaskEventRepository.findByTaskId(task.getId())))
+                .collect(Collectors.toList());
     }
 
     private static class TaskDoneBeforeDeadline {
@@ -66,6 +85,32 @@ public class ReportController {
                     .getOccurrence();
 
             this.doneBeforeDeadline = deadline.isBefore(dateOfLastChange) && done;
+        }
+    }
+
+    @GetMapping("/doneBeforeDeadline")
+    List<TaskDoneBeforeDeadlineWithCounter> readAllTasksDoneBeforeDeadline(){
+        var result  = persistedTaskEventRepository.findAllDoneBeforeDeadline();
+        var target = result.stream().map(persistedTaskEvent -> {
+            var description = taskRepository.findById(persistedTaskEvent.taskId).stream()
+                    .map(Task::getDescription).collect(Collectors.toList()).get(0);
+            var lastChange = persistedTaskEvent.getOccurrence();
+
+            return new TaskDoneBeforeDeadlineWithCounter(description, lastChange, persistedTaskEventRepository.findByTaskId(persistedTaskEvent.taskId));
+        })
+                .collect(Collectors.toList());
+        return target;
+    }
+
+    private static class TaskDoneBeforeDeadlineWithCounter {
+        public String description;
+        public LocalDateTime lastChange;
+        public int changesCounter;
+
+        TaskDoneBeforeDeadlineWithCounter(String description, LocalDateTime lastChange, final List<PersistedTaskEvent> events){
+            this.description = description;
+            this.lastChange = lastChange;
+            this.changesCounter = events.size();
         }
     }
 }
